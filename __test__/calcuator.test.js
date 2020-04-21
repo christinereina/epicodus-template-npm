7969a1c170056b2dd73b4e495ba358288f42a71a
Christine Augustine <blacchoy@gmail.com>
2020-04-21 05:15:34 -0700

    first test successfully passed, confirming age

--- __test__/calcuator.test.js
+++ __test__/calcuator.test.js
@@ -4,6 +4,6 @@ describe ('User', () => {
   let newUser = new User(29);
 
   test ('this should test that the users age is stored and read as a number', () => {
-    expect(newUser.age).toEqual(0);
+    expect(newUser.age).toEqual(29);
   });
 });
