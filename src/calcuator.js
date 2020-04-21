8f0da65df1ec786eff2d5c0242afaaab76fcc7a9
Christine Augustine <blacchoy@gmail.com>
2020-04-21 05:03:28 -0700

    add constructor for age only

--- src/calcuator.js
+++ src/calcuator.js
@@ -1,9 +1,6 @@
-export class age {
-  constructor () {
-    this.earthAge
-    this.mercuryAge
-    this.venusAge
-    this.marsAge
-    this.jupiterAge
+export class User {
+  constructor (age) {
+    this.age = age;
   }
-  }
+
+}
