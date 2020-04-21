3c83f043f63a6db80f7f9b81a37b03f6616b1775
Christine Augustine <blacchoy@gmail.com>
2020-04-21 06:00:08 -0700

    write fourth test for mars age calculation, designed to fail

--- __test__/calcuator.test.js
+++ __test__/calcuator.test.js
@@ -6,13 +6,19 @@ describe ('User', () => {
     expect(newUser.age).toEqual(29);
   });
 
-  test ('this should test mercury age calculation', () => {
+  test ('this should test Mercury age calculation', () => {
     let newUser = new User(29);
     expect(newUser.mercuryAge()).toEqual(120);
   });
 
-  test ('this should test venus age calculation', () => {
+  test ('this should test Venus age calculation', () => {
     let newUser = new User(29);
     expect(newUser.venusAge()).toEqual(46);
   });
+
+  test ('this should test Mars age calculation', () => {
+    let newUser = new User(29);
+    expect(newUser.marsAge()).toEqual(46);
+  });
+
 });
