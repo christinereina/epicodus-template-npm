f5fcb459f0609b26e84447f11e5195be78855e80
Christine Augustine <blacchoy@gmail.com>
2020-04-21 05:01:41 -0700

    write first test for users age, meant to fail

---
+++ __test__/calcuator.test.js
@@ -0,0 +1,9 @@
+import { User } from "./src/calcuator";
+
+describe ('User', () => {
+  let newUser = new User(29);
+
+  test ('this should test that the users age is stored and read as a number', () => {
+    expect(newUser.age).toEqual(0);
+  }
+}
