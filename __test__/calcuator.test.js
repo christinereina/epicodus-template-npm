523b1abbe0fe137bc08672d634cc31bc344bb818
Christine Augustine <blacchoy@gmail.com>
2020-04-21 05:11:56 -0700

    first test failed successfully

--- __test__/calcuator.test.js
+++ __test__/calcuator.test.js
@@ -1,9 +1,9 @@
-import { User } from "./src/calcuator";
+import { User } from "../src/calcuator.js";
 
 describe ('User', () => {
   let newUser = new User(29);
 
   test ('this should test that the users age is stored and read as a number', () => {
     expect(newUser.age).toEqual(0);
-  }
-}
+  });
+});
